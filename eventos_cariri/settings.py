6bf4c1e2bb582a69e08dd70291cd104d9cf12c11
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:26:31 -0300

    feat: template dir added to path

--- eventos_cariri/settings.py
+++ eventos_cariri/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'eventos_cariri.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['eventos_cariri/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
