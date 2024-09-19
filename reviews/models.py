ed468ce7e6ae2071ab0677b844a722006491e8ae
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:29:59 -0300

    feat: review model created

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from events.models import Event
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Review(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.event.name} - {self.rating}'
