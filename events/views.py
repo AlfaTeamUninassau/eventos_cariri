0d00b13974936cca080991347f864a3ab8563002
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:28:32 -0300

    feat: create view for event and home

--- events/views.py
+++ events/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from events.models import Event
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = Event
+    template_name = 'home.html'
+    context_object_name = 'home'
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'events/event_list.html'
+    context_object_name = 'events'
+    paginate_by = 10
+    
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+    context_object_name = 'event'
+
+
+
