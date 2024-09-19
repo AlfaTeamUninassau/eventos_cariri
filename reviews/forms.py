ee4e742b8d11f4c3fa2cd910e2eeabd951b60211
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:28:42 -0300

    feat: form to review

---
+++ reviews/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.RadioSelect(choices=[(i, i) for i in range(1, 6)]),
+        }
