f0c1e2fe40e478d9bce69f4462bd66a961b3b3f9
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:26:17 -0300

    feat: view to create a comment

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
+from .models import Comment
 
-# Create your views here.
+
+class CommentsCreateView(CreateView):
+    model = Comment
+    template_name = 'comments/comments.html'
+    fields = ['comment']
+    success_url = reverse_lazy('comments')
+    context_object_name = 'comments'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
