a8433349006a4819338af51bf1ec96b2515b31b0
Azinth <gsv.jesus@gmail.com>
2024-09-19 14:25:35 -0300

    feat: creat Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
