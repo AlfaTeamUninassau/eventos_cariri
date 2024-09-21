1213f75ad798a6300196e5378dcc2454cf6849b5
Azinth <gsv.jesus@gmail.com>
2024-09-21 19:36:14 -0300

    feat: new route to a new event

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import EventListView
+from .views import (
+    EventListView,
+    EventDetailView,
+    EventCreateView,
+    # EventUpdateView,
+    # EventDeleteView
+    )
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events'),
+    path('<int:pk>/', EventDetailView.as_view(), name='event_detail'),
+    path('new/', EventCreateView.as_view(), name='event_create'),
+    # path('<int:pk>/edit/', EventUpdateView.as_view(), name='event_update'),
+    # path('<int:pk>/delete/', EventDeleteView.as_view(), name='event_delete'),
 ]
