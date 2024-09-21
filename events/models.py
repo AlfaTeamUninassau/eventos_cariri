8a7e67add60befffe8374681a68b4925b8c4efba
Azinth <gsv.jesus@gmail.com>
2024-09-21 19:36:04 -0300

    fix: new field to event

--- events/models.py
+++ events/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
-# Create your models here.
 
+from django.db import models
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateTimeField()
-    location = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='events/images/')
+    time = models.TimeField(null=True, blank=True)  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+
+class EventImage(models.Model):
+    event = models.ForeignKey(Event, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='events/images/')
